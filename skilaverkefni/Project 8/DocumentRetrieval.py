ff046fd970765237009051215d9dca9a0a3cf5be
jon_ingi <jonio18@ru.is>
2018-10-19 22:50:26 +0000

    prog 8 update

--- skilaverkefni/Project 8/DocumentRetrieval.py
+++ skilaverkefni/Project 8/DocumentRetrieval.py
@@ -14,8 +14,7 @@ def open_file(filename):
                     continue
                 line = line.split()
                 for word in line:
-                    stripped = word.lower()
-                    word_list[counter].append(stripped)
+                    word_list[counter].append(word)
         return word_list
     except FileNotFoundError:
         print("Documents not found")
@@ -40,31 +39,47 @@ def quit_the_program():
 
 def search_documents(word_list):
     search_word_dict = {}
+    value_set = set([])
     try:
         search_word = input("Enter search words: ").lower()  # .join(str(x) for x in L)
-        counter = 0
-        for x, my_list in enumerate(word_list):
+        if ' ' in search_word:
+            target1, target2 = search_word.split()
+            search_key = target1, target2
+        else:
+            search_key = search_word
+        for x, my_list in enumerate(word_list, start=-1):
             if not my_list:
                 continue
             for word in my_list:
                 lower_word = word.lower().strip(string.punctuation)
-                search_word_dict.setdefault(lower_word, []).append(x)
-                counter += 1
-            print("Documents that fit search:", ' '.join(map(str, set(search_word_dict[search_word]))))
+                if word in search_key:
+                    search_word_dict.setdefault(lower_word, []).append(x)
+        for values in search_word_dict.values():
+            value_set.update(values)
+        if len(value_set) == 0:
+            print("No match.")
+        else:
+            print("Documents that fit search:", *value_set)
     except KeyError:
         print("No match.")
 
 
 def print_documents(word_list):
-    doc = int(input("Enter document number: "))
-    print("Document #{}".format(doc))
-    print(word_list[doc + 1], end=' ')
+    try:
+        doc = int(input("Enter document number: "))
+        print("Document #{}".format(doc))
+        print(*word_list[doc + 1], end=' ')
+    except IndexError:
+        print("No such document")
+        quit()
 
 
 def main():
     filename = input("Document collection: ")
-    data_file = open_file(filename)
-    file_options(data_file)
+    data_read = True
+    while data_read:
+        data_file = open_file(filename)
+        file_options(data_file)
 
 
 main()
