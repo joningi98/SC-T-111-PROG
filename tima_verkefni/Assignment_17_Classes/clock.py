fb48e8ec7ffc61ca69d9253a98b48b0871a29d3e
jon_ingi <jonio18@ru.is>
2018-10-23 11:05:59 +0000

    clock

--- tima_verkefni/Assignment_17_Classes/clock.py
+++ tima_verkefni/Assignment_17_Classes/clock.py
@@ -1,21 +1,23 @@
 class Clock(object):
-    def __init__(self, hours='0', minutes='0', seconds='0'):
+    def __init__(self, hours=0, minutes=0, seconds=0):
         self.hours = hours
         self.minutes = minutes
         self.seconds = seconds
 
-    def __str__(self):
-        return str(self.hours + ':' + self.minutes + ':' + self.seconds)
-
     def str_update(self, update):
         hh, mm, ss = update.split(':')
-        self.hours = hh
-        self.minutes = mm
-        self.seconds = ss
+        self.hours = int(hh)
+        self.minutes = int(mm)
+        self.seconds = int(ss)
 
+    def add_clocks(self, clock):
+        s_dif, s = divmod((self.seconds + clock.seconds), 60)
+        m_dif, m = divmod((self.minutes + clock.minutes + s_dif), 60)
+        h = (self.hours + clock.hours + m_dif) % 24
+        return Clock(h, m, s)
 
-    def add_clocks(self):
-        pass
+    def __str__(self):
+        return "{} hours, {} minutes and {} seconds".format(self.hours, self.minutes, self.seconds)
 
 
 clock1 = Clock()
@@ -25,3 +27,5 @@ print(clock2)
 clock1.str_update("03:21:34")
 clock2.str_update("05:45:52")
 print(clock1)
+clock3 = clock1.add_clocks(clock2)
+print(clock3)
