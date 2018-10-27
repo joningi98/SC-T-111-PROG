d7879758590218b4f28de51743c8ecc4199414d8
joningi98 <joningi17201@gmail.com>
2018-10-27 00:12:36 +0000

    123

---
+++ venv/Scripts/easy_install-3.7-script.py
@@ -0,0 +1,12 @@
+#!"C:\Users\Jón Ingi\Documents\GitHub\SC-T-111-PROG\venv\Scripts\python.exe" -x
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==39.1.0','console_scripts','easy_install-3.7'
+__requires__ = 'setuptools==39.1.0'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('setuptools==39.1.0', 'console_scripts', 'easy_install-3.7')()
+    )
